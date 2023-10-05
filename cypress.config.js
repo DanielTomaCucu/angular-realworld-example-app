2d5852519cc3db8894400b8f94c62a794f4c07c6
Cucu Toma Daniel <cucutomadaniel@192-168-0-239.rdsnet.ro>
2023-10-05 09:32:47 +0300

    add custom scripts  and env

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,12 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   video: false,
+  env: {
+    userName: 'daniel@d.com',
+    password: "123456",
+    apiUrl:"https://api.realworld.io",
+  },
+  
   e2e: {
     baseUrl: "http://localhost:4200",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
